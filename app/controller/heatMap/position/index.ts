34492f241e51cda7e8c2a64b24797cb0f65a0ed5
MTY <tianyuan.mao@zebra-c.com>
2019-08-06 10:54:01 +0800

    fix

--- app/controller/heatMap/position/index.ts
+++ app/controller/heatMap/position/index.ts
@@ -1,145 +1,145 @@
-import { Controller } from 'egg';
-import * as xlsx from 'xlsx';
-import { getView } from '../../../view/heatMap/position/position-has-pointer';
-import { getViewAmap3D } from '../../../view/heatMap/position/position-has-pointer-amap-3d';
-import { getViewAmap } from '../../../view/heatMap/position/position-has-pointer-amap';
-import { HEAMP_MARKER_ICON } from '../../../lib/utils';
+import { Controller } from 'egg'
+import * as xlsx from 'xlsx'
+import { getView } from '../../../view/heatMap/position/position-has-pointer'
+import { getViewAmap3D } from '../../../view/heatMap/position/position-has-pointer-amap-3d'
+import { getViewAmap } from '../../../view/heatMap/position/position-has-pointer-amap'
+import { HEAMP_MARKER_ICON } from '../../../lib/utils'
 
-const PAGE_TAG = 'heatMap';
-const TYPE = 'position';
-const isSelfProvince = false; // 是否只获取本省内Excel数据，默认全部
+const PAGE_TAG = 'heatMap'
+const TYPE = 'position'
+const isSelfProvince = false // 是否只获取本省内Excel数据，默认全部
 
 export default class PositionController extends Controller {
   /**
    * 生成文件
    */
   public async createPath() {
-    const { ctx } = this;
-    const { type = '' } = this.ctx.query;
-    let handlerFormatFun = getView;
+    const { ctx, service } = this
+    const { type = '' } = ctx.query
+    let handlerFormatFun = getView
     if (type === 'amap') {
-      handlerFormatFun = getViewAmap;
+      handlerFormatFun = getViewAmap
     } else if (type === 'amap3D') {
-      handlerFormatFun = getViewAmap3D;
+      handlerFormatFun = getViewAmap3D
     }
-    const data = await this.service.excel.getExcelsData({ folderName: PAGE_TAG, type: TYPE, handlerFormat: this.formatData }); // 获取PAGE_TAG文件夹下，所有Excel格式化后数据
-    await this.service.fileAsync.writeFilesHTML({ data, folderName: PAGE_TAG, type: TYPE, templateView: handlerFormatFun }); // 生成对应html文件
-    await this.service.fileAsync.writeFilesJS({ data, folderName: PAGE_TAG, type: TYPE }); // 生成js文件
-    ctx.body = 'success';
-    ctx.status = 200;
+    const data = await service.excel.getExcelsData({ folderName: PAGE_TAG, type: TYPE, handlerFormat: this.formatData }) // 获取PAGE_TAG文件夹下，所有Excel格式化后数据
+    await service.fileAsync.writeFilesHTML({ data, folderName: PAGE_TAG, type: TYPE, templateView: handlerFormatFun }) // 生成对应html文件
+    await service.fileAsync.writeFilesJS({ data, folderName: PAGE_TAG, type: TYPE }) // 生成js文件
+    ctx.body = 'success'
+    ctx.status = 200
   }
   /**
    * 生成文件并且下载压缩文件 @TODO 出错
    */
   public async createPathDown() {
-    const { ctx } = this;
-    const { type = '' } = this.ctx.query;
-    let handlerFormatFun = getView;
+    const { ctx } = this
+    const { type = '' } = ctx.query
+    let handlerFormatFun = getView
     if (type === 'amap') {
-      handlerFormatFun = getViewAmap;
+      handlerFormatFun = getViewAmap
     } else if (type === 'amap3D') {
-      handlerFormatFun = getViewAmap3D;
+      handlerFormatFun = getViewAmap3D
     }
-    const data = await this.service.excel.getExcelsData({ folderName: PAGE_TAG, type: TYPE, handlerFormat: this.formatData }); // 获取PAGE_TAG文件夹下，所有Excel格式化后数据
-    console.log('--------write---html----begin-----');
-    await this.service.fileAsync.writeFilesHTML({ data, folderName: PAGE_TAG, type: TYPE, templateView: handlerFormatFun }); // 生成对应html文件
-    console.log('--------write---js----begin-----');
-    await this.service.fileAsync.writeFilesJS({ data, folderName: PAGE_TAG, type: TYPE }); // 生成js文件
-    ctx.body = 'test';
-    ctx.status = 200;
+    const data = await this.service.excel.getExcelsData({ folderName: PAGE_TAG, type: TYPE, handlerFormat: this.formatData }) // 获取PAGE_TAG文件夹下，所有Excel格式化后数据
+    console.log('--------write---html----begin-----')
+    await this.service.fileAsync.writeFilesHTML({ data, folderName: PAGE_TAG, type: TYPE, templateView: handlerFormatFun }) // 生成对应html文件
+    console.log('--------write---js----begin-----')
+    await this.service.fileAsync.writeFilesJS({ data, folderName: PAGE_TAG, type: TYPE }) // 生成js文件
+    ctx.body = 'test'
+    ctx.status = 200
   }
   /**
    * 压缩文件
    */
   public async compress() {
-    const { ctx } = this;
-    const content = await this.service.file.compressDir({ ctx, folderName: PAGE_TAG, type: TYPE, isDel: true }); // 压缩文件后将文件返回给服务器,并删除目标文件和压缩文件
-    ctx.body = content;
-    ctx.status = 200;
+    const { ctx } = this
+    const content = await this.service.file.compressDir({ ctx, folderName: PAGE_TAG, type: TYPE, isDel: true }) // 压缩文件后将文件返回给服务器,并删除目标文件和压缩文件
+    ctx.body = content
+    ctx.status = 200
   }
   /**
    * 生成网页json
    */
   public async getJSON() {
-    const { ctx } = this;
-    const data = await this.service.excel.getExcelsData({ folderName: PAGE_TAG, type: TYPE, handlerFormat: this.formatData }); // 获取PAGE_TAG文件夹下，所有Excel格式化后数据const rows = await this.service.excel.getExcelsData({ folderName: PAGE_TAG, handlerFormat: this.formatData });
+    const { ctx } = this
+    const data = await this.service.excel.getExcelsData({ folderName: PAGE_TAG, type: TYPE, handlerFormat: this.formatData }) // 获取PAGE_TAG文件夹下，所有Excel格式化后数据const rows = await this.service.excel.getExcelsData({ folderName: PAGE_TAG, handlerFormat: this.formatData });
     ctx.body = {
       code: 200,
       data,
       success: true,
-    };
-    ctx.status = 200;
+    }
+    ctx.status = 200
   }
   /**
    * 下载示例模版文件
    */
   public async downTemplateFile() {
-    const { ctx } = this;
-    const fileName = '居住常访地-带点.zip';
-    const content = await this.service.file.readTemplateFile({ ctx, folderName: PAGE_TAG, fileName });
-    ctx.body = content;
-    ctx.status = 200;
+    const { ctx } = this
+    const fileName = '居住常访地-带点.zip'
+    const content = await this.service.file.readTemplateFile({ ctx, folderName: PAGE_TAG, fileName })
+    ctx.body = content
+    ctx.status = 200
   }
   formatData({ sheets, fileName }) {
     const positions = sheets.map((sheet, index) => {
       if (sheet && index === 0) {
-        return getPosition({ sheet, fileName }, isSelfProvince);
+        return getPosition({ sheet, fileName }, isSelfProvince)
       } else if (sheet && index === 1) {
-        return getPointer({ sheet, icon: HEAMP_MARKER_ICON.blue });
+        return getPointer({ sheet, icon: HEAMP_MARKER_ICON.blue })
       } else if (sheet && index === 2) {
-        return getPointer({ sheet, icon: HEAMP_MARKER_ICON.pink });
+        return getPointer({ sheet, icon: HEAMP_MARKER_ICON.pink })
       }
-    });
-    return positions;
+    })
+    return positions
   }
 }
 interface SeetJSONConfig {
-  lon: any;
-  lat: any;
-  RATE: any;
-  PROVINCE: any;
-  CITY: any;
-  [TYPE: string]: any;
+  lon: any
+  lat: any
+  RATE: any
+  PROVINCE: any
+  CITY: any
+  [TYPE: string]: any
 }
 
 interface SeetPointerConfig {
-  gdlon: any;
-  gdlat: any;
-  [NAME: string]: any;
+  gdlon: any
+  gdlat: any
+  [NAME: string]: any
 }
 function getPosition({ sheet, fileName }, self?: boolean) {
-  const sheetJSON: SeetJSONConfig[] = xlsx.utils.sheet_to_json(sheet);
-  const heatMap = {};
-  const rateObj = {};
-  const [ TYPE, RATE, PROVINCE, CITY ] = [ '位置信息', '覆盖占比', '省', '市' ];
-  const max = {};
-  const selfProvince = sheetJSON && sheetJSON[0][PROVINCE];
+  const sheetJSON: SeetJSONConfig[] = xlsx.utils.sheet_to_json(sheet)
+  const heatMap = {}
+  const rateObj = {}
+  const [ TYPE, RATE, PROVINCE, CITY ] = [ '位置信息', '覆盖占比', '省', '市' ]
+  const max = {}
+  const selfProvince = sheetJSON && sheetJSON[0][PROVINCE]
   if (self) {
-    console.log('------本省-----');
+    console.log('------本省-----')
     sheetJSON.forEach(item => {
-      const { lon, lat, [RATE]: rate, [PROVINCE] : province, [TYPE]: type } = item;
+      const { lon, lat, [RATE]: rate, [PROVINCE] : province, [TYPE]: type } = item
       if (selfProvince === province) {
-        const tempHeatMapArr = heatMap[type] = heatMap[type] || [];
-        const tempHeatRateArr = rateObj[type] = rateObj[type] || [];
-        tempHeatRateArr.push(rate);
-        tempHeatMapArr.push([ lon, lat, rate ]);
+        const tempHeatMapArr = heatMap[type] = heatMap[type] || []
+        const tempHeatRateArr = rateObj[type] = rateObj[type] || []
+        tempHeatRateArr.push(rate)
+        tempHeatMapArr.push([ lon, lat, rate ])
       }
-    });
+    })
   } else {
-    console.log('------所有-----');
+    console.log('------所有-----')
     sheetJSON.forEach(item => {
-      const { gdlon, gdlat, [RATE]: rate, [TYPE]: type } = item;
-      const tempHeatMapArr = heatMap[type] = heatMap[type] || [];
-      const tempHeatRateArr = rateObj[type] = rateObj[type] || [];
-      tempHeatRateArr.push(rate);
-      tempHeatMapArr.push([ gdlon, gdlat, rate ]);
-    });
+      const { gdlon, gdlat, [RATE]: rate, [TYPE]: type } = item
+      const tempHeatMapArr = heatMap[type] = heatMap[type] || []
+      const tempHeatRateArr = rateObj[type] = rateObj[type] || []
+      tempHeatRateArr.push(rate)
+      tempHeatMapArr.push([ gdlon, gdlat, rate ])
+    })
   }
 
   Object.keys(rateObj).forEach(key => {
-    const newArray: any[] = Array.from(new Set(rateObj[key]));
-    max[`${key}`] = Math.max(...newArray);
-  });
+    const newArray: any[] = Array.from(new Set(rateObj[key]))
+    max[`${key}`] = Math.max(...newArray)
+  })
   return Object.assign({
     radius: 20,
     province: selfProvince,
@@ -147,18 +147,18 @@ function getPosition({ sheet, fileName }, self?: boolean) {
     fileName,
     heatMap,
     max,
-  });
+  })
 }
 function getPointer({ sheet, icon }) {
-  const sheetJSON: SeetPointerConfig[] = xlsx.utils.sheet_to_json(sheet);
-  const pointer: any[] = [];
-  const NAME = '店名';
+  const sheetJSON: SeetPointerConfig[] = xlsx.utils.sheet_to_json(sheet)
+  const pointer: any[] = []
+  const NAME = '店名'
   sheetJSON.forEach(item => {
-    const { gdlon, gdlat, [NAME]: name } = item;
-    pointer.push([ gdlon, gdlat, name ]);
-  });
+    const { gdlon, gdlat, [NAME]: name } = item
+    pointer.push([ gdlon, gdlat, name ])
+  })
   return Object.assign({
     pointer,
     icon,
-  });
+  })
 }
--- app/service/file.ts
+++ app/service/file.ts
@@ -1,58 +1,75 @@
-import { Service } from 'egg';
-import { createReadStream, unlink, existsSync, readdirSync, statSync, unlinkSync, rmdirSync } from 'fs';
-import { resolve } from 'path';
-import * as compressing from 'compressing';
-const EXPORT_FOLDER = 'export'; // 导出js html文件名
-const DOWN_TEMPLATE_FOLER = 'template'; // 导出js html文件名
+import { Service } from 'egg'
+import { createReadStream, unlink, existsSync, readdirSync, statSync, unlinkSync, rmdirSync } from 'fs'
+import { resolve } from 'path'
+import * as compressing from 'compressing'
+const EXPORT_FOLDER = 'export' // 导出js html文件名
+const DOWN_TEMPLATE_FOLER = 'template' // 导出js html文件名
+
+interface ZipConfig {
+  ctx: any
+  fileName: string, // 压缩后目录名称
+  targetZipFile: string // 压缩后文件放置文件目录
+  sourceFolder: string // 源文件需要压缩的文件
+  isDel?: boolean // 压缩后是否删除
+}
+interface CompressConfig {
+  ctx: any
+  folderName: string // 模块文件
+  type: string // 模块文件类型
+  isDel?: boolean
+}
+interface TemplateConfig {
+  ctx: any
+  folderName: string // 模块文件
+  fileName: string // 压缩后目录名称
+}
 
 export default class FileService extends Service {
-  public async readTemplateFile({ ctx, folderName, fileName }) {
-    ctx.attachment(fileName);
-    const filePath = resolve(ctx.app.config.static.dir, folderName, DOWN_TEMPLATE_FOLER, fileName); // 示例 path： path/template/index.zip
-    ctx.set('Content-Type', 'application/octet-stream');
-    return await createReadStream(filePath);
+  public async readTemplateFile({ ctx, folderName, fileName }: TemplateConfig) {
+    ctx.attachment(fileName)
+    const filePath: string = resolve(ctx.app.config.static.dir, folderName, DOWN_TEMPLATE_FOLER, fileName) // 示例 path： path/template/index.zip
+    ctx.set('Content-Type', 'application/octet-stream')
+    return await createReadStream(filePath)
   }
   /**
    * 读取压缩包
-   * @param {filePath isDel}  文件路径 读完是否删除
    */
-  private async _readZipDel({ ctx, targetZipFile, isDel = false, sourceFolder }) {
-    ctx.set('Content-Type', 'application/octet-stream');
+  private async _readZipDel({ ctx, fileName, targetZipFile, isDel = false, sourceFolder }: ZipConfig) {
+    ctx.attachment(fileName)
+    ctx.set('Content-Type', 'application/octet-stream')
     return await createReadStream(targetZipFile).on('close', () => {
-      if (!isDel) return;
+      if (!isDel) return
       unlink(targetZipFile, err => {
-        if (err) throw err;
-        console.log('---------success---delete------zip------');
-        this.delFolder(sourceFolder); // 删除源文件
-      });
-    });
+        if (err) throw err
+        console.log('---------success---delete------zip------')
+        this.delFolder(sourceFolder) // 删除源文件
+      })
+    })
   }
   /**
    * 生成压缩包
-   * @param {ctx folderName, type} 上下文 目录 类型
    */
-  public async compressDir({ ctx, folderName, type, isDel }) {
-    const sourceFolder = resolve(ctx.app.config.static.dir, folderName, EXPORT_FOLDER, type); // 源文件需要压缩的文件
-    const fileName: string = `${type}.zip`;
-    const targetZipFile: string = resolve(ctx.app.config.static.dir, folderName, EXPORT_FOLDER, fileName); // 压缩后文件放置文件目录
-    await compressing.zip.compressDir(sourceFolder, targetZipFile);
-    ctx.attachment(fileName);
-    console.log('--------compress---ok---------');
-    return await this._readZipDel({ ctx, targetZipFile, isDel, sourceFolder });
+  public async compressDir({ ctx, folderName, type, isDel }: CompressConfig) {
+    const sourceFolder: string = resolve(ctx.app.config.static.dir, folderName, EXPORT_FOLDER, type)
+    const fileName: string = `${type}.zip`
+    const targetZipFile: string = resolve(ctx.app.config.static.dir, folderName, EXPORT_FOLDER, fileName)
+    await compressing.zip.compressDir(sourceFolder, targetZipFile)
+    console.log('--------compress---ok---------')
+    return await this._readZipDel({ ctx, fileName, targetZipFile, isDel, sourceFolder })
   }
-  public async delFolder(path) {
-    let files: any[] = [];
+  public async delFolder(path: string) {
+    let files: string[] = []
     if (existsSync(path)) {
-      files = readdirSync(path);
+      files = readdirSync(path)
       files.forEach(file => {
-        const curPath = path + '/' + file;
+        const curPath = path + '/' + file
         if (statSync(curPath).isDirectory()) {
-          this.delFolder(curPath); // 递归删除文件夹
+          this.delFolder(curPath) // 递归删除文件夹
         } else {
-          unlinkSync(curPath); // 删除文件
+          unlinkSync(curPath) // 删除文件
         }
-      });
-      rmdirSync(path); // 删除目录
+      })
+      rmdirSync(path) // 删除目录
     }
   }
 }
