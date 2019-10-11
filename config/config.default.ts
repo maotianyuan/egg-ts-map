7d96c2a8ee2cce50accabb7736cc546598b43737
ReliaMM <1820184044@qq.com>
2019-10-11 21:30:52 +0800

    build

--- config/config.default.ts
+++ config/config.default.ts
@@ -43,8 +43,7 @@ export default (appInfo: EggAppInfo) => {
     dialect: 'mysql',
     host: '127.0.0.1',
     port: 3306,
-    password: 'root',
-    // password: 'password@#!',
+    password: 'password@#!',
     database: 'share',
   }
   // the return config will combines to EggAppConfig
